26da1641f23241f8fc17ccc4ba25e0bf879518ee
berlovesumwah <166808321+weberbers201938@users.noreply.github.com>
2024-10-26 13:50:55 +0800

    Update codm.js

--- fb/commands/codm.js
+++ fb/commands/codm.js
@@ -1,29 +1,31 @@
-const axios = require('axios');
+const axios = require("axios");
+const name = "codm";
 
 module.exports = {
-  name: 'codm',
-  description: 'Fetch a CODM video link from the API and send it as a video',
-  async execute(senderId, args, pageAccessToken, sendMessage) {
+  name,
+  description: "fetch random codm video",
+  async run({ api, send }) {
+    send("This will take a few minutes, please wait...");
+
     try {
       const response = await axios.get('http://37.114.46.139:6065/codm');
-      const videoUrl = response.data.url;
+      const url = response.data.url;
 
-      if (videoUrl) {
-        sendMessage(senderId, {
-          attachment: {
-            type: 'video',
-            payload: {
-              url: videoUrl,
-            },
-          },
-        }, pageAccessToken);
-      } else {
-        sendMessage(senderId, { text: 'No video URL found.' }, pageAccessToken);
-      }
+      if (!url) throw new Error("No video URL found.");
+
+      await send({
+        attachment: {
+          type: "video",
+          payload: {
+            url,
+            is_reusable: true
+          }
+        }
+      });
+      
+      send(`Video created successfully!\nDownload link: ${url}`);
     } catch (error) {
-      console.error('Error fetching CODM video:', error);
-      sendMessage(senderId, { text: 'There was an error fetching the video.' }, pageAccessToken);
+      send("Error while generating your request. Please try again.\n" + (error.message || error));
     }
-  },
+  }
 };
-  
